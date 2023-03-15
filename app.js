a30cf27c5fff35b03ff768f4685868248203d142
Covstar <okechukwuvictor093@gmail.com>
2023-03-15 15:53:08 +0100

    Updated using tailwind

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const path = require ('path');
 
 
 const app = express();
-       app.set ('views', path.join (__dirname, 'views'));
-        app.use (express.static (__dirname + '/public'));
+       app.set('views', path.join(__dirname, 'views'));
+        app.use(express.static(path.join(__dirname, '/public')));
 
 
         app.use(bodyParser.urlencoded({extended: true}));
